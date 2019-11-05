844dd1114115a12e395153946d6aea7d2707b0bb
xerardoo <gerardoo_2413@hotmail.com>
2019-11-04 23:59:23 -0700

    app, hero delete button added

--- app/src/views/Heroes/Hero.vue
+++ app/src/views/Heroes/Hero.vue
@@ -1,71 +1,80 @@
 <template>
-    <div class="row">
-        <div class="col-md-2">
-            <div class="level">
-                <b class="d-block">Level</b>
-                <font-awesome-icon icon="star"/>
-                <font-awesome-icon icon="star"/>
-                <font-awesome-icon icon="star"/>
-                <font-awesome-icon icon="star"/>
-                <font-awesome-icon icon="star"/>
-            </div>
-            <div class="stats">
-                <b class="d-block">Stats</b>
-                <span class="badge badge-warning">Strength 0</span> <br>
-                <span class="badge badge-info">Intelligence 0</span> <br>
-                <span class="badge badge-success">Dexterity 0</span> <br>
+    <div>
+        <div class="row">
+            <div class="col-md-1 offset-md-9">
+                <button class="btn btn-danger" @click="onDelete">Delete</button>
             </div>
         </div>
-
-        <div class="col-md-10">
-            <form @submit="onSubmit">
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="firstName" class="col-form-label">First Name:</label>
-                        <input type="text" class="form-control" id="firstName" v-model="form.firstname" placeholder=""
-                               required>
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <label for="lastName" class="col-form-label">Last Name:</label>
-                        <input type="text" class="form-control" id="lastName" v-model="form.lastname" placeholder="">
-                    </div>
+        <div class="row">
+            <div class="col-md-2">
+                <div class="level">
+                    <b class="d-block">Level</b>
+                    <font-awesome-icon icon="star"/>
+                    <font-awesome-icon icon="star"/>
+                    <font-awesome-icon icon="star"/>
+                    <font-awesome-icon icon="star"/>
+                    <font-awesome-icon icon="star"/>
                 </div>
-
-                <div class="form-row">
-                    <div class="form-group col-md-4">
-                        <label for="race">Race:</label>
-                        <select class="form-control" v-model="form.racesid" name="" id="race">
-                            <option v-for="race in races" v-bind:value="race.id" :key="race.id">
-                                {{race.name}}
-                            </option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="class">Class:</label>
-                        <select class="form-control" v-model="form.classesid" name="" id="class">
-                            <option v-for="clas in classes" v-bind:value="clas.id" :key="clas.id">
-                                {{clas.name}}
-                            </option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="weapon">Weapon:</label>
-                        <select class="form-control" v-model="form.weaponsid" name="" id="weapon">
-                            <option v-for="weapon in weapons" v-bind:value="weapon.id" :key="weapon.id">
-                                {{weapon.name}}
-                            </option>
-                        </select>
-                    </div>
+                <div class="stats">
+                    <b class="d-block">Stats</b>
+                    <span class="badge badge-warning">Strength 0</span> <br>
+                    <span class="badge badge-info">Intelligence 0</span> <br>
+                    <span class="badge badge-success">Dexterity 0</span> <br>
                 </div>
+            </div>
 
-                <div class="form-group row">
-                    <div class="offset-md-9 col-md-3">
-                        <button type="reset" class="btn btn-secondary">Discard</button>
-                        <button type="submit" class="btn btn-primary ml-1">Save</button>
+            <div class="col-md-10">
+                <form @submit="onSubmit">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="firstName" class="col-form-label">First Name:</label>
+                            <input type="text" class="form-control" id="firstName" v-model="form.firstname"
+                                   placeholder=""
+                                   required>
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="lastName" class="col-form-label">Last Name:</label>
+                            <input type="text" class="form-control" id="lastName" v-model="form.lastname"
+                                   placeholder="">
+                        </div>
                     </div>
-                </div>
-            </form>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-4">
+                            <label for="race">Race:</label>
+                            <select class="form-control" v-model="form.racesid" name="" id="race">
+                                <option v-for="race in races" v-bind:value="race.id" :key="race.id">
+                                    {{race.name}}
+                                </option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="class">Class:</label>
+                            <select class="form-control" v-model="form.classesid" name="" id="class">
+                                <option v-for="clas in classes" v-bind:value="clas.id" :key="clas.id">
+                                    {{clas.name}}
+                                </option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="weapon">Weapon:</label>
+                            <select class="form-control" v-model="form.weaponsid" name="" id="weapon">
+                                <option v-for="weapon in weapons" v-bind:value="weapon.id" :key="weapon.id">
+                                    {{weapon.name}}
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="offset-md-9 col-md-3">
+                            <button type="reset" class="btn btn-secondary">Discard</button>
+                            <button type="submit" class="btn btn-primary ml-1">Save</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </template>
@@ -128,6 +137,18 @@
                     })
                     .catch(error => console.log(error))
             },
+            onDelete() {
+                let res = confirm('Are you sure ?');
+                if (res) {
+                    Axios.delete(`${this.APIHOST}/heroes/${this.form.id}`)
+                        .then(response => {
+                            console.log(response.data);
+                            alert("deleted");
+                            history.back();
+                        })
+                        .catch(error => console.log(error));
+                }
+            },
             onSubmit(e) {
                 e.preventDefault();
 
@@ -144,6 +165,7 @@
                         .then(response => {
                             console.log(response.data);
                             alert("success");
+                            history.back();
                         })
                         .catch(error => console.log(error));
                 else
@@ -151,6 +173,7 @@
                         .then(response => {
                             console.log(response.data);
                             alert("success");
+                            history.back();
                         })
                         .catch(error => console.log(error));
             }
